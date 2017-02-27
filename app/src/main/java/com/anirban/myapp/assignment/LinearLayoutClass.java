c822f7f5e2d721e8bf499478f99f270fca6d1eda
anirban2017 <anirbanmit1980@gmail.com>
2017-02-27 21:09:43 +0530

    change App name and launcher Icon

---
+++ app/src/main/java/com/anirban/myapp/assignment/LinearLayoutClass.java
@@ -0,0 +1,24 @@
+package com.anirban.myapp.assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayoutClass extends AppCompatActivity {
+
+
+    /*In manifest file Replace the another class &
+    also explain the learners about Launcher & Default Activity Practically
+    As Event Handling & Intents are not introduced yet.*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_example);
+
+        /**
+         * Explain linear layout design by introducing weight concept practically in the same file
+         * set height to 0 dp
+         * weight as 1
+         */
+    }
+}
---
+++ app/src/main/java/com/anirban/myapp/assignment/RelativeLayoutClass.java
@@ -0,0 +1,17 @@
+package com.anirban.myapp.assignment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by me on 6/21/2016.
+ */
+public class RelativeLayoutClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_example);
+    }
+}
